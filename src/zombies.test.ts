a3f1a68a5211e12f7dec2f628f46b985bc4a6e0c
defycab <love.bellinder@gmail.com>
2024-09-09 16:17:43 +0200

    first test

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -1,30 +1,35 @@
-import { ok } from "node:assert/strict";
-import { test } from "node:test";
+import { ok } from "node:assert/strict"
+import { test } from "node:test"
 
 const createRoom = (capacity: number) => {
-  const _capacity = capacity;
+  const _capacity = capacity
 
+  if (_capacity === 0) {
+    return {
+      isFull: () => true,
+    }
+  }
   return {
-    isFull: () => true,
-  };
-};
+    isFull: () => false,
+  }
+}
 
 test("room is not full", () => {
-  const room = createRoom(0);
+  const room = createRoom(0)
 
-  const result = room.isFull();
+  const result = room.isFull()
 
-  ok(result);
-});
+  ok(result)
+})
 
-test.skip("empty room that fits one zombie is not full", () => {});
+test.skip("empty room that fits one zombie is not full", () => {})
 
-test.skip("empty room cannot fit any zombies", () => {});
+test.skip("empty room cannot fit any zombies", () => {})
 
-test.skip("one-roomer becomes full when a zombie is added", () => {});
+test.skip("one-roomer becomes full when a zombie is added", () => {})
 
-test.skip("two-roomer is not full when a zombie is added", () => {});
+test.skip("two-roomer is not full when a zombie is added", () => {})
 
-test.skip("second zombie consumes first zombie when added to a one-roomer", () => {});
+test.skip("second zombie consumes first zombie when added to a one-roomer", () => {})
 
 // You are free to add more tests that you think are relevant!
