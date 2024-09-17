362cf4b47701f05642696b7e79f56d3cdc9e6de6
defycab <love.bellinder@gmail.com>
2024-09-17 14:26:40 +0200

    rename variable for readability

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -17,9 +17,9 @@ const createRoom = (capacity: number) => {
 test("room is not full", () => {
   const room = createRoom(0)
 
-  const result = room.isFull()
+  const isRoomFull = room.isFull()
 
-  ok(result)
+  ok(isRoomFull)
 })
 
 test.skip("empty room that fits one zombie is not full", () => {})
